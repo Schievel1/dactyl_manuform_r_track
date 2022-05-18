440a8bb37bf25aafcba3f8706780134f0e55d77d
Pascal Jäger <pascal.jaeger@kleinknecht.de>
2022-05-18 16:53:56 +0200

    Added keymaps, worked on qmk instructions

--- qmk-firmware/keyboards/handwired/dactyl_manuform_r_track/keymaps/colemak/keymap.c
+++ qmk-firmware/keyboards/handwired/dactyl_manuform_r_track/keymaps/colemak/keymap.c
@@ -20,7 +20,7 @@
 
 #include QMK_KEYBOARD_H
 
-#define _DVORAK 0
+#define _COLEMAKDHM 0
 #define _LOWER 1
 #define _RAISE 2
 
@@ -193,36 +193,45 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-[_DVORAK] = LAYOUT_5x6(
-KC_GRV,     KC_1,    KC_2,    KC_3 ,    KC_4 ,    KC_5,                  KC_6    ,    KC_7 ,       KC_8 ,      KC_9 ,      KC_0        ,KC_EQL,
-KC_TAB,     KC_QUOT, KC_COMM, KC_DOT  , KC_P    , KC_Y   ,               KC_F    ,    KC_G       , KC_C      , KC_R      , KC_L ,       KC_SLSH,
-KC_ESC,     KC_A ,   KC_O   , KC_E    , KC_U    , KC_I   ,               KC_D    ,    KC_H       , KC_T      , KC_N      , KC_S        ,KC_MINS,
-KC_NO,      KC_SCLN, KC_Q   , KC_J    , KC_K    , KC_X   ,               KC_B    ,    KC_M       , KC_W   ,    KC_V    ,   KC_Z     ,   KC_BSLS,
-                    KC_RBRC, KC_RBRC,                                                       KC_PGUP, KC_PGDN,
-                                       TD(GUI_TM),    TD(SFT_TM),        _______, KC_RSFT,
-                                       TD(CTL_TM),    KC_SPC,            _______, KC_BSPC,
-                                       TD(ALT_TM),    TD(LOW_TM),        KC_ENT, KC_RALT
+//[_COLEMAK] = LAYOUT_5x6(
+//DM_REC1,    DM_REC1, DM_PLY1, DM_REC2 , DM_PLY2 , DM_RSTP,               KC_CPI_DOWN, KC_CPI_STD , KC_CPI_UP , KC_SMO_SC , KC_0        ,KC_QUOT_MY,
+//KC_TAB,     KC_Q ,   KC_W   , KC_F    , KC_P    , KC_G   ,               KC_J    ,    KC_L       , KC_U      , KC_Y      , KC_SCLN_INV ,KC_QUOT_MY,
+//KC_ESC,     KC_A ,   KC_R   , KC_S    , KC_T    , KC_D   ,               KC_H    ,    KC_N       , KC_E      , KC_I      , KC_O        ,KC_MINS,
+//KC_TILD_MY, KC_Z ,   KC_X   , KC_C    , KC_V    , KC_B   ,               KC_K    ,    KC_M       , KC_COMM   , KC_DOT    , KC_SLSH     ,KC_BSLASH,
+//                           _______, _______,                                         _______, KC_LGUI,
+//                                       TD(SFT_TM),    TD(RAI_TM),          _______, KC_SPC,
+//                                       TD(CTL_TM),    TD(GUI_TM),          _______, KC_ENT,
+//                                       TD(ALT_TM),    TD(LOW_TM),          KC_BSPC, KC_DEL
+//),
+[_COLEMAKDHM] = LAYOUT_5x6(
+DM_REC1,    DM_REC1, DM_PLY1, DM_REC2 , DM_PLY2 , DM_RSTP,               KC_CPI_DOWN, KC_CPI_STD , KC_CPI_UP , KC_SMO_SC , KC_0        ,KC_QUOT_MY,
+KC_TAB,     KC_Q ,   KC_W   , KC_F    , KC_P    , KC_B   ,               KC_J    ,    KC_L       , KC_U      , KC_Y      , KC_SCLN_INV ,KC_QUOT_MY,
+KC_ESC,     KC_A ,   KC_R   , KC_S    , KC_T    , KC_G   ,               KC_M    ,    KC_N       , KC_E      , KC_I      , KC_O        ,KC_MINS,
+KC_TILD_MY, KC_Z ,   KC_X   , KC_C    , KC_D    , KC_V   ,               KC_K    ,    KC_H       , KC_COMM   , KC_DOT    , KC_SLSH     ,KC_BSLASH,
+                           _______, _______,                                         _______, KC_LGUI,
+                                       TD(SFT_TM),    TD(RAI_TM),          _______, KC_SPC,
+                                       TD(CTL_TM),    TD(GUI_TM),          _______, KC_ENT,
+                                       TD(ALT_TM),    TD(LOW_TM),          KC_BSPC, KC_DEL
 ),
 
 [_LOWER] = LAYOUT_5x6(
 
-KC_TILD,     KC_F1,    KC_F2,    KC_F3,         KC_F4,     KC_F5,        KC_F6,   KC_F7,     KC_F8,     KC_F9,       KC_F10,     KC_DEL ,
-_______,    _______, _______,  _______,       _______,   KC_LCBR,        KC_RCBR, KC_BTN1,   _______,   _______  ,   _______ ,   _______,
-_______,    _______, _______,  TD(RAI_TM),     KC_DEL,   KC_LPRN,        KC_RPRN, KC_LEFT ,  KC_UP,     KC_DOWN,     KC_RGHT,    KC_PIPE,
-KC_CAPS ,   _______, _______,  LCTL(KC_X), LCTL(KC_C),LCTL(KC_V),        _______, KC_BTN2  , _______,   _______,     _______,    _______,
-                    _______,_______,                                                                  _______,_______,
-                                        _______,_______,                 _______,_______,
-                                        _______,_______,                 _______,_______,
-                                        _______,_______,                 _______,_______
+KC_TILD, KC_EXLM     , KC_AT   , KC_HASH   , KC_DLR  ,KC_PERC,           KC_CIRC, KC_AMPR , KC_ASTR    , KC_LPRN  , KC_RPRN   ,           _______,
+_______, KC_PGDN     , KC_HOME , KC_END    , KC_PGUP ,_______,           _______, _______ , RALT(KC_Y) , _______  , RALT(LSFT(KC_SCLN)) , _______,
+_______, RALT(KC_Q)  , _______ ,RALT(KC_S) , KC_RBRC ,_______,           KC_BTN3, KC_BTN1 , RALT(KC_5) , KC_BTN2  , RALT(KC_P),           _______,
+KC_F12 , KC_F1       , KC_F2   , KC_F3     , KC_F4   , KC_F5 ,           KC_F6  , KC_F7   , KC_F8      , KC_F9    , KC_F10    ,           KC_F11 ,
+                            _______,_______,                             _______,_______,
+                                        _______,_______,             _______,_______,
+                                        _______,_______,             _______,_______,
+                                        _______,_______,             _______,_______
 ),
 
 [_RAISE] = LAYOUT_5x6(
-
-_______, _______, _______, _______, _______, _______,                    _______, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, KC_CALC,
-_______, _______, _______, _______, _______, KC_LBRC,                    KC_RBRC, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, KC_MUTE,
-_______, _______, _______, _______, _______, KC_LPRN,                    KC_RPRN, KC_P4,   KC_P5,   KC_P6,   _______, KC_VOLU,
-_______, _______, _______, _______, _______, _______,                    KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PEQL, KC_VOLD,
-                             _______,_______,                                KC_KCDOT, KC_COMM,
+_______, _______ , _______ , _______ , _______ ,_______,                 _______, _______      , _______      , _______     , _______     , _______,
+_______,  KC_1   , KC_2    , KC_3    , KC_4    , KC_5  ,                 KC_6   , KC_7         , KC_8         , KC_9        , KC_0        , KC_QUOT,
+_______, KC_LPRN , KC_RPRN , KC_LBRC , KC_RBRC ,KC_LBRC,                 KC_RBRC, LSFT(KC_LBRC),LSFT(KC_RBRC) ,LSFT(KC_COMM),LSFT(KC_DOT) ,KC_MS_BTN3,
+KC_TILD, KC_EXLM , KC_AT   , KC_HASH , KC_DLR  ,KC_PERC,                 KC_CIRC, KC_AMPR      , KC_ASTR      , KC_PLUS     , KC_EQL      , KC_DEL,
+                             _______,_______,                                _______,_______,
                                         _______,_______,             _______,_______,
                                         _______,_______,             _______,_______,
                                         _______,_______,             _______,_______
